f86a8db113a3105cb09af898fff57ee86d44f2d8
Shamilika <IT22030962@gmail.com>
2025-04-30 00:16:08 +0530

    implement try catch error in the ordercontroller

--- backend/Src/controllers/orderController.js
+++ backend/Src/controllers/orderController.js
@@ -2,18 +2,44 @@ const Order = require('../models/orderModel');
 
 exports.createOrder = async (req, res) => {
   try {
-    const { userId, cartItems, paymentIntentId, paymentStatus, totalAmount, shippingAddress } = req.body;
+    const {
+      userId,
+      cartItems,
+      paymentIntentId,
+      paymentStatus,
+      totalAmount,
+      shippingAddress,
+    } = req.body;
 
     // Validate required fields
-    if (!userId || !cartItems || cartItems.length === 0 || !paymentIntentId || !totalAmount || isNaN(totalAmount) || !paymentStatus) {
-      return res.status(400).json({ message: 'Invalid or missing order details' });
+    if (
+      !userId ||
+      !cartItems ||
+      cartItems.length === 0 ||
+      !paymentIntentId ||
+      !totalAmount ||
+      isNaN(totalAmount) ||
+      !paymentStatus
+    ) {
+      return res
+        .status(400)
+        .json({ message: 'Invalid or missing order details' });
     }
 
-    
+    const newOrder = new Order({
+      userId,
+      cartItems,
+      totalAmount,
+      paymentIntentId,
+      paymentStatus,
+      orderDate: new Date(),
+      shippingAddress,
+    });
 
     await newOrder.save();
-    res.status(201).json({ message: 'Order created successfully', order: newOrder });
-
+    res
+      .status(201)
+      .json({ message: 'Order created successfully', order: newOrder });
   } catch (error) {
     console.error('Error creating order:', error);
     res.status(500).json({ message: 'Error placing order. Please try again.' });
@@ -24,8 +50,8 @@ exports.createOrder = async (req, res) => {
 exports.getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find()
-      .populate('userId', 'firstname lastname email phone')  // Populate userId with firstname, lastname, email, and phone
-      .populate('cartItems.productId', 'name price');  // Populate product data for each item in the cart
+      .populate('userId', 'firstname lastname email phone') // Populate userId with firstname, lastname, email, and phone
+      .populate('cartItems.productId', 'name price'); // Populate product data for each item in the cart
 
     if (!orders || orders.length === 0) {
       return res.status(404).json({ message: 'No orders found' });
@@ -34,7 +60,8 @@ exports.getAllOrders = async (req, res) => {
     res.status(200).json({ orders });
   } catch (error) {
     console.error('Error fetching orders:', error);
-    res.status(500).json({ message: 'Error fetching orders. Please try again.' });
+    res
+      .status(500)
+      .json({ message: 'Error fetching orders. Please try again.' });
   }
 };
-
