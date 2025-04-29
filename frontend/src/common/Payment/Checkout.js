5241df4cfe17173477ef9ea3c5a471a40b4b930a
Shamilika <IT22030962@gmail.com>
2025-04-29 23:59:15 +0530

    Changes in the form

--- frontend/src/common/Payment/Checkout.js
+++ frontend/src/common/Payment/Checkout.js
@@ -317,6 +317,17 @@ const cardElementStyles = {
   fontSize: '18px',
 };
 
+const payButtonStyles = {
+  backgroundColor: '#004f9a',
+  color: 'white',
+  padding: '16px',
+  width: '100%',
+  border: 'none',
+  borderRadius: '4px',
+  fontSize: '18px',
+  cursor: 'pointer',
+};
+
 const statusStyles = {
   marginTop: '20px',
   textAlign: 'center',
