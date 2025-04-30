cea9e0b4eff0b74bc5fcc04b94388b19e5c8f480
Shamilika <IT22030962@gmail.com>
2025-04-30 18:43:00 +0530

    Try catch added in the adim side  of payment

--- web/src/com/Category/AddCat.js
+++ web/src/com/Category/AddCat.js
@@ -30,12 +30,7 @@ const ItemForm = ({ form, onFinish = () => {} }) => {
     }
 
     try {
-      setIsUploading(true);
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        'divensignature@gmail.com',
-        '12345678'
-      );
+     
       const user = userCredential.user;
 
       const storageRef = ref(storage, `categories/${file.name}`);
